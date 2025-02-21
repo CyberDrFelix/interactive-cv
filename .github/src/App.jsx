e34d3f5e5eb31d56f7c91936adb76d69869fe289
CyberDrFelix <felixburcea@yahoo.com>
2025-02-21 09:38:20 +0100

    Create App.jsx

---
+++ .github/src/App.jsx
@@ -0,0 +1,11 @@
+import InteractiveCV from './components/InteractiveCV.jsx'  // Added .jsx extension
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <InteractiveCV />
+    </main>
+  )
+}
+
+export default App
